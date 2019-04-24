4f2a4d54a179b6389503c13da86f1dab59e08581
xujinsheng <1156551536@qq.com>
2019-04-24 19:39:26 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- index
+    <el-button type="danger">危险按钮</el-button>
+    <el-alert
+      title="成功提示的文案"
+      type="success">
+    </el-alert> -->
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+  mounted(){
+    // axios会返回promise对象，对象下面有then这方法
+    this.$axios({
+      // 路径
+      url: "https://api.github.com/users",
+      // 请求方式
+      method: "GET",
+      // get请求参数
+      params: {},
+    }).then(res => {
+      console.log(res)
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from "axios"
+
+import VueRouter from "vue-router"
+
+// 1.element - 引入
+import ElementUI from "element-ui";
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 导入组件
+import Login from "./pages/Login.vue"
+
+// 2.element - 注册
+Vue.use(ElementUI)
+// 注册路由
+Vue.use(VueRouter);
+
+// 路由的配置
+const routes = [
+  {path: "/login", component: Login}
+]
+
+// 创建路由对象
+const router = new VueRouter({routes})
+
+// 是否是生成环境
+Vue.config.productionTip = false;
+
+Vue.prototype.$axios = axios;
+
+new Vue({
+  router,
+  render: h => h(App),
+  // $mount作用相当于el，根实例挂载到节点
+}).$mount('#app')
